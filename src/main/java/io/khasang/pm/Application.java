ea76b94a7516595cb3f205c88668fd8c64b86290
skuznet <azon.sk@gmail.com>
2019-04-22 21:40:00 +0300

    add new thread example with new branch development

--- src/main/java/io/khasang/pm/Application.java
+++ src/main/java/io/khasang/pm/Application.java
@@ -1,7 +1,17 @@
 package io.khasang.pm;
 
+import java.util.concurrent.TimeUnit;
+
 public class Application {
     public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from " + Thread.currentThread().getName());
+        }).start();
         System.out.println("Hello!");
     }
 }
