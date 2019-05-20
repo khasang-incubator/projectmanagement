bad4c05e713567fd7d190842233f4e411db88081
skuznet <azon.sk@gmail.com>
2019-05-20 21:57:37 +0300

    add spring security

--- src/main/java/io/khasang/pm/controller/AppController.java
+++ src/main/java/io/khasang/pm/controller/AppController.java
@@ -2,8 +2,10 @@ package io.khasang.pm.controller;
 
 import io.khasang.pm.model.CreateTable;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -20,6 +22,12 @@ public class AppController {
         this.rabbit = rabbit;
     }
 
+
+    @RequestMapping("/admin")
+    public String getAdminPage() {
+        return "admin";
+    }
+
     @RequestMapping("/")
     public String getHelloPage(Model model) {
 
@@ -29,9 +37,23 @@ public class AppController {
     }
 
     @RequestMapping("/create")
-    public String createTable(Model model){
-        model.addAttribute("status",createTable.getTableCreationStatus());
+    public String createTable(Model model) {
+        model.addAttribute("status", createTable.getTableCreationStatus());
         return "create";
     }
+
+    @RequestMapping("/user")
+    public String getUserPage() {
+        return "user";
+    }
+
+    // localhost:8080/password/admin
+    @RequestMapping("/password/{password}")
+    public String getEncryptPassword(@PathVariable("password") String password, Model model) {
+        model.addAttribute("password", password);
+        model.addAttribute("passwordAfterEncode", new BCryptPasswordEncoder().encode(password));
+        return "password";
+    }
+
 
 }
