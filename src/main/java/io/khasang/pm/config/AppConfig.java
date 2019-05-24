3883a752a207ea590f7fdcd93bddfd8efffa8278
Ryzvanov Renat <renat2029@mail.ru>
2019-05-24 17:41:35 +0300

    Add hibernate

--- src/main/java/io/khasang/pm/config/AppConfig.java
+++ src/main/java/io/khasang/pm/config/AppConfig.java
@@ -1,6 +1,5 @@
 package io.khasang.pm.config;
 
-import io.khasang.pm.controller.Cat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
