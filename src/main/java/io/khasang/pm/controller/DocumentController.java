871010829cb780c8569b78c444a07937ca51f76c
SVN <serg_vn@inbox.ru>
2019-06-17 21:46:13 +1000

    refractoring of DocumentController & Document

--- src/main/java/io/khasang/pm/controller/DocumentController.java
+++ src/main/java/io/khasang/pm/controller/DocumentController.java
@@ -5,7 +5,11 @@ import io.khasang.pm.entity.Document;
 import io.khasang.pm.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
