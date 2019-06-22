fc737a2ef74520faab7925208ac9a88a4742e7cc
mskomarov <mx123@yandex.ru>
2019-06-22 12:35:56 +1000

    Added javadoc for method add() into interface ChildDocumentService

--- src/main/java/io/khasang/pm/service/ChildDocumentService.java
+++ src/main/java/io/khasang/pm/service/ChildDocumentService.java
@@ -5,6 +5,12 @@ import io.khasang.pm.entity.ChildDocument;
 import java.util.List;
 
 public interface ChildDocumentService {
+    /**
+     * required for adding childDocument to db
+     *
+     * @param childDocument - childDocument for adding
+     * @return added childDocument
+     */
     ChildDocument add(ChildDocument childDocument);
 
     ChildDocument update(ChildDocument childDocument);
